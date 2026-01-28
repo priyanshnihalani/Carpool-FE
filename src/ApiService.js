85aac081d13e46c0ffb30e59187bb3b34898717d
priyanshnihalani <priyansh.nihalani@gmail.com>
2026-01-28 12:10:11 +0530

    [PN][Api configuration changed]

--- src/ApiService.js
+++ src/ApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: "https://carpool-api.techrover.us/",
 });
 
 export const ApiService = {
